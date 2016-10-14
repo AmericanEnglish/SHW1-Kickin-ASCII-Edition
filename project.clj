ad4ccb83f2d8cd5684cabd8ebab0676614d0182f
storm9789 <jreinsmith@olivetcollege.org>
2016-10-13 20:37:23 -0400

    The base

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject shw1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot shw1.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
