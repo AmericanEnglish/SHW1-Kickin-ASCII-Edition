c064c2583f48b1f4326486dcf83bb16d9004295b
Carlos Barajas <12barajasc@gmail.com>
2016-10-16 18:09:54 -0400

    auto lower cases the leading "command" for input

--- src/shw1/core.clj
+++ src/shw1/core.clj
@@ -4,12 +4,19 @@
   (:require [resources.rooms :refer :all])
 )
 
+(defn sanitize_input 
+  [input]
+  (let [splitt (clojure.string/split input #" ")]
+    (apply list (clojure.string/lower-case (nth splitt 0))
+           (drop 1 splitt))
+  )
+)
 
 (defn handler 
   "Handles the users commands, dispatching them to and fro"
   [input commands]
   ; This is obviously filler code for now
-  (let [parsed (clojure.string/split input #" ")]
+  (let [parsed (sanitize_input input)]
     (let [result (search_command_name (nth parsed 0) commands)]
       (if (not (empty? result))
         ((:fn result) (clojure.string/join " " (drop 1 parsed)))
