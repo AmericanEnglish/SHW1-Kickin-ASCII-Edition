15fb2997b13a687033b9b2c3ebdeebf6846dc85c
Carlos Barajas <12barajasc@gmail.com>
2016-10-26 18:49:35 -0400

    for designing functions while we add to the room templates

---
+++ src/resources/workspace.clj
@@ -0,0 +1,17 @@
+(ns resources.workspace)
+
+(defn gen_room 
+  "Generates a room given an room template, and a room ID"
+  [template room_id]
+  (nil) ; some function here 
+)
+
+(defn gen_new 
+  "Generates a room by passing a randomly selected template to gen_room"
+  [room_id]
+  (gen_room 
+      (nil) ; Some function here  
+      room_id
+  )
+)
+
