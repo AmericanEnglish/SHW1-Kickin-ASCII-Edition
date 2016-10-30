7419bd8540a9f0448b8b24bd504923b9da513b26
Carlos Barajas <12barajasc@gmail.com>
2016-10-30 19:14:55 -0400

    small ` typo

--- src/resources/workspace.clj
+++ src/resources/workspace.clj
@@ -3,7 +3,7 @@
 (defn gen_room 
   "Generates a room given an room template, and a room ID"
   [template room_id]
-  (let [sub (rand-nth (:subclass template))]`
+  (let [sub (rand-nth (:subclass template))]
     (list
     	(hash-map :id room_id :name (str (:type sub) (:basename template)) :description (:description sub) :exits (vector))
     	(repeat (:total_exits sub) room_id)
