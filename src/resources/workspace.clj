eedd0a02cc229c0b1dd217c404d90375c85ef423
storm9789 <jreinsmith@olivetcollege.org>
2016-10-30 19:49:59 -0400

    put room templates into workspace

--- src/resources/workspace.clj
+++ src/resources/workspace.clj
@@ -1,22 +1,194 @@
 (ns resources.workspace)
 
-(defn gen_room 
-  "Generates a room given an room template, and a room ID"
-  [template room_id]
-  (let [sub (rand-nth (:subclass template))]
-    (list
-    	(hash-map :id room_id :name (str (:type sub) (:basename template)) :description (:description sub) :exits (vector))
-    	(repeat (:total_exits sub) room_id)
+
+(def room_templates 
+  (list
+    (hash-map 
+      :basename "example"
+      :subclass 
+        (list 
+          (hash-map 
+            :type "size_example"
+            :total_exits 10
+            :description "example description here"
+          )
+          (hash-map
+            :type "other_example"
+            :total_exits 25
+          )
+        )
+    )
+    (hash-map 
+      :basename "EXAMPLE"
+      :subclass 
+        (list 
+          (hash-map 
+            :type "SIZE_EXAMPLE"
+            :total_exits 5
+            :description "EXAMPLE DESCRPTION HERE"
+          )
+        )
     )
   )
 )
+   
 
-(defn gen_new 
-  "Generates a room by passing a randomly selected template to gen_room"
-  [all_templates room_id]
-  (gen_room 
-      (rand-nth all_templates) 
-      room_id
+(def room_spec
+  (list
+      (hash-map 
+        :basename "Bathroom"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Master"
+              :total_exits 2
+              :description ""
+            )
+            (hash-map
+              :type "Full"
+              :total_exits 1
+              :description ""
+            )
+            (hash-map
+              :type "Half"
+              :total_exits 1
+              :description ""
+            )
+          )
+      )
+      (hash-map 
+        :basename "Bedroom"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Master"
+              :total_exits 2
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "Guest"
+              :total_exits 1
+              :description "test_descrip_med"
+            )
+            (hash-map
+              :type "Child"
+              :total_exits 1
+              :description "test_descrip_small"
+            )
+          )
+      )
+      (hash-map 
+        :basename "foyer"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Front"
+              :total_exits 2
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "Back"
+              :total_exits 2
+              :description "test_descrip_med"
+            )
+          )
+      )
+      (hash-map 
+        :basename "Garage"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "3-Car"
+              :total_exits 1
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "2-Car"
+              :total_exits 
+              :description "test_descrip_med"
+            )
+            (hash-map
+              :type "1-Car"
+              :total_exits 
+              :description "test_descrip_small"
+            )
+          )
+      )
+      (hash-map 
+        :basename "Kitchen"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Full"
+              :total_exits 4
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "Half"
+              :total_exits 2
+              :description "test_descrip_med"
+            )
+
+          )
+      )
+      (hash-map 
+        :basename "Office"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Large"
+              :total_exits 2
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "Medeium"
+              :total_exits 1
+              :description "test_descrip_med"
+            )
+            (hash-map
+              :type "Small"
+              :total_exits 1
+              :description "test_descrip_small"
+            )
+          )
+      )
+      (hash-map 
+        :basename "Pantry"
+        :subclass 
+          (list 
+            (hash-map
+              :type "Large"
+              :total_exits 1
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "small"
+              :total_exits 1
+              :description "test_descrip_med"
+            )
+          )
+      )
+      (hash-map 
+        :basename "Parlour"
+        :subclass 
+          (list 
+            (hash-map 
+              :type "Dining"
+              :total_exits 4
+              :description "test_descrip_large"
+            )
+            (hash-map
+              :type "Family"
+              :total_exits 4
+              :description "test_descrip_med"
+            )
+            (hash-map
+              :type "Living"
+              :total_exits 4
+              :description "test_descrip_small"
+            )
+          )
+      )
+      
   )
 )
-
