7221c6ac654934759a237a04e22180d60f142569
Grant <Garras@olivetcollege.edu>
2016-11-15 19:53:13 -0500

    Added a break messege for when items break and to be removed

--- src/resources/item_templates.clj
+++ src/resources/item_templates.clj
@@ -8,12 +8,14 @@
             :description "You might think it bizzare but in this universe ALL fireaxes are actually comprised of nothing less than actual fire and raven feathers. It's standard issue directly from the Interdimensional Home Explorers Committee."
             :verb "potential door murder(s)" 
             :amount 1
+            :break "Your Fireaxe has shattered into a million pieces.\nI hope you brought some keys..."
 		)
 		(hash-map
 			:name "Rock"
 			:description "A petoskey stone the size of a baseball."
 			:verb "smashes"
 			:amount 1
+			:break "You broke the Rock."
 		)
 	)
 )
